a06a1e736fbbf396f68f075439c58b190620fa6c
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-10-06 00:51:19 +0300

    anlık saat bilgisinin alınması için gerekli kod yapısı oluşturuldu

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,14 @@
-et userName = prompt("Adınızı Giriniz")
+let userName = prompt("Adınızı Giriniz")
 let info = document.querySelector("#myName")
 info.innerHTML = `${userName}`
 info.classList.add("bg-dark","text-center","text1-text-center");
+
+function localtime()
+{
+  
+    let date = new Date().toLocaleString()
+    let zaman = document.querySelector("#myClock")
+    zaman.innerHTML = ` ${date}  `
+    zaman.classList.add("clock")
+}
+setInterval(localtime,1000)
